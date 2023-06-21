f7f2f4bb3a98a0cce1178378bbdc903ff571f3d7
Joalor64 <rhgarcon@gmail.com>
2023-06-20 20:53:21 -0400

    Update Init.hx

--- source/Init.hx
+++ source/Init.hx
@@ -11,110 +11,110 @@ import flixel.util.FlxTimer;
 class Init extends FlxState
 {
     public static var muteKeys:Array<FlxKey> = [FlxKey.ZERO];
-	public static var volumeDownKeys:Array<FlxKey> = [FlxKey.NUMPADMINUS, FlxKey.MINUS];
-	public static var volumeUpKeys:Array<FlxKey> = [FlxKey.NUMPADPLUS, FlxKey.PLUS];
+    public static var volumeDownKeys:Array<FlxKey> = [FlxKey.NUMPADMINUS, FlxKey.MINUS];
+    public static var volumeUpKeys:Array<FlxKey> = [FlxKey.NUMPADPLUS, FlxKey.PLUS];
 
     var epicSprite:FlxSprite;
 
     public function new() 
-	{
-		super();
+    {
+	super();
 
-		persistentUpdate = true;
-		persistentDraw = true;
-	}
+	persistentUpdate = true;
+	persistentDraw = true;
+    }
 
     override function create()
     {
         Paths.clearStoredMemory();
-		Paths.clearUnusedMemory();
+	Paths.clearUnusedMemory();
 
-		var bg:FlxSprite = new FlxSprite().makeGraphic(FlxG.width, FlxG.height, FlxColor.WHITE);
+	var bg:FlxSprite = new FlxSprite().makeGraphic(FlxG.width, FlxG.height, FlxColor.WHITE);
         bg.scale.set(10, 10);
-		bg.screenCenter();
+	bg.screenCenter();
         add(bg);
 
         epicSprite = new FlxSprite().loadGraphic(Paths.image('logo')); // placeholder for now
         epicSprite.antialiasing = ClientPrefs.globalAntialiasing;
         epicSprite.angularVelocity = 30;
-		epicSprite.screenCenter();
+	epicSprite.screenCenter();
         add(epicSprite);
         
         load();
 
         new FlxTimer().start(4, function(timer) 
-		{
-			startGame();
-		});
+	{
+		startGame();
+	});
 
         super.create();
     }
 
     override function update(elapsed)
-	{
-		if (FlxG.keys.justPressed.ENTER || FlxG.keys.justPressed.SPACE)
-			skip();
+    {
+	if (FlxG.keys.justPressed.ENTER || FlxG.keys.justPressed.SPACE)
+		skip();
 
-		super.update(elapsed);
-	}
+	super.update(elapsed);
+    }
 
     function load()
     {
         #if LUA_ALLOWED
-		Paths.pushGlobalMods();
-		#end
+	Paths.pushGlobalMods();
+	#end
         // Just to load a mod on start up if ya got one. For mods that change the menu music and bg
-		WeekData.loadTheFirstEnabledMod();
+	WeekData.loadTheFirstEnabledMod();
 
-		FlxG.game.focusLostFramerate = 60;
+	FlxG.game.focusLostFramerate = 60;
 
-		FlxG.sound.muteKeys = muteKeys;
-		FlxG.sound.volumeDownKeys = volumeDownKeys;
-		FlxG.sound.volumeUpKeys = volumeUpKeys;
+	FlxG.sound.muteKeys = muteKeys;
+	FlxG.sound.volumeDownKeys = volumeDownKeys;
+	FlxG.sound.volumeUpKeys = volumeUpKeys;
 		
-		FlxG.keys.preventDefaultKeys = [TAB];
+	FlxG.keys.preventDefaultKeys = [TAB];
 
-		PlayerSettings.init();
+	PlayerSettings.init();
 
         FlxG.save.bind('bobverse', 'fitbvteam');
 		
-		ClientPrefs.loadPrefs();
+	ClientPrefs.loadPrefs();
 		
-		Highscore.load();
+	Highscore.load();
 
         if(!initialized && FlxG.save.data != null && FlxG.save.data.fullscreen)
-		{
-			FlxG.fullscreen = FlxG.save.data.fullscreen;
-		}
+	{
+		FlxG.fullscreen = FlxG.save.data.fullscreen;
+	}
 
-		if (FlxG.save.data.weekCompleted != null)
-		{
-			StoryMenuState.weekCompleted = FlxG.save.data.weekCompleted;
-		}
+	if (FlxG.save.data.weekCompleted != null)
+	{
+		StoryMenuState.weekCompleted = FlxG.save.data.weekCompleted;
+	}
 
-		FlxG.mouse.visible = false;
+	FlxG.mouse.visible = false;
         
-		#if desktop
-		if (!DiscordClient.isInitialized)
-		{
-			DiscordClient.initialize();
-			Application.current.onExit.add (function (exitCode) {
-				DiscordClient.shutdown();
-			});
-		}
-		#end
+	#if desktop
+	if (!DiscordClient.isInitialized)
+	{
+		DiscordClient.initialize();
+		Application.current.onExit.add (function (exitCode) {
+			DiscordClient.shutdown();
+		});
+	}
+	#end
     }
 
     function skip() 
-	{
-		startGame();
-	}
+    {
+	startGame();
+    }
 
-	function startGame() 
-	{
+    function startGame() 
+    {
         FlxG.camera.fade(FlxColor.BLACK, 0.33, false, function() 
         {
-			FlxG.switchState(new TitleState());
-	    });
-	}
+		FlxG.switchState(new TitleState());
+	});
+    }
 }
